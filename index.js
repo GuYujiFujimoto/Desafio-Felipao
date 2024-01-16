e55706201aba37ba7e83535c762f68eaca3f0d44
GuYujiFujimoto <yujifujimoto@outlook.com.br>
2024-01-16 13:27:34 -0300

    Atualizacao

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-AAAAAAAA;
+let nome = "Gustavo";
+let quantidadeDeXP = 11000
+let nivel;
+
+if (quantidadeDeXP <= 1000){
+    nivel = "Ferro";
+} else if (quantidadeDeXP >= 1001 && quantidadeDeXP <= 2000){
+    nivel = "Bronze";
+} else if (quantidadeDeXP >= 2001 && quantidadeDeXP <= 5000){
+    nivel = "Prata";
+} else if (quantidadeDeXP >= 5001 && quantidadeDeXP <= 7000){
+    nivel = "Ouro";
+} else if (quantidadeDeXP >= 7001 && quantidadeDeXP <= 8000){
+    nivel = "Platina";
+} else if (quantidadeDeXP >= 8001 && quantidadeDeXP <= 9000){
+    nivel = "Ascendente";
+} else if (quantidadeDeXP >= 9001 && quantidadeDeXP <= 10000){
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log (`O Herói ${nome} está no nível: (${nivel})`);
